9308351883582f5a85ab7d702469a175d952be59
DavideViolante <fungtq3@gmail.com>
2020-04-13 23:03:25 +0200

    Parametrize endpoint

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 require('dotenv').config();
 const express = require('express');
 
-const getUsers = require('./server/github');
+const getUsers = require('./routes/github');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 
-app.get('/', getUsers, (req, res) => res.render('index', { users: res.locals.data }));
+app.get('/', (req, res) => res.json('Please call GET /:owner/:repo/:endpoint - See README.md for more info'));
+app.get('/:owner/:repo/:endpoint', getUsers, showIndex);
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(port, () => console.log(`Repository Famous Users listening on port ${port}`));
+
+function showIndex(req, res) {
+  res.render('index', { users: res.locals.data });
+}
